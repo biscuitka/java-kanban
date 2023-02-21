e5829a98345c77ef130c0ac8b6321b5a9762b01e
biscuitka <biscuitka@ya.ru>
2023-02-21 17:46:20 +0300

    изменить/добавить тесты

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,7 @@ public class Main {
         System.out.println();
 
         /**
-         * создание 1го эпика с 2мя подзадачами
+         * создание 1го эпика с 3мя подзадачами
          */
         Epic epicTaskOne = new Epic();
         epicTaskOne.setName("Эпик1");
@@ -55,43 +55,40 @@ public class Main {
         subtaskTwo.setEpicTask(epicTaskOne);
         epicTaskOne.getSubTasks().add(subtaskTwo);
 
+        SubTask subTaskThree = new SubTask();
+        subTaskThree.setName("подзадача3 эпика2");
+        subTaskThree.setDescription("описание подзадачи3");
+        manager.createSubtask(subTaskThree);
+        subTaskThree.setEpicTask(epicTaskOne);
+        epicTaskOne.getSubTasks().add(subTaskThree);
+
         /**
-         * создание 2го эпика с 1й подзадачей
+         * создание 2го эпика без подзадач
          */
         Epic epicTaskTwo = new Epic();
         epicTaskTwo.setName("Эпик2");
         epicTaskTwo.setDescription("описание эпика2");
         manager.createEpic(epicTaskTwo);
 
-        SubTask subTaskThree = new SubTask();
-        subTaskThree.setName("подзадача3 эпика2");
-        subTaskThree.setDescription("описание подзадачи3");
-        manager.createSubtask(subTaskThree);
-
-        subTaskThree.setEpicTask(epicTaskTwo);
-        epicTaskTwo.getSubTasks().add(subTaskThree);
-
-
         System.out.println("тест создания epicTask - " + "\n" + epicTaskOne + "\n" + epicTaskTwo);
         System.out.println();
 
-        System.out.println("тест создания subTask - " + "\n" + subtaskOne + "\n" + subtaskTwo);
+        System.out.println("тест создания subTask - " + "\n" + subtaskOne + "\n" + subtaskTwo + "\n" + subTaskThree);
         System.out.println();
 
 
         /**
-         * тестирование сохранения, получения списков, обновления и удаления задач.
+         * тестирование сохранения, получения списков, истории, обновления и удаления задач.
          */
         System.out.println("тест сохранения задач " + "\n" + manager.getListOfTasks() +
                 "\n" + manager.getListOfEpicTasks() + "\n" + manager.getListOfSubTasks());
         System.out.println();
 
+
         System.out.println("обновляем задачу");
         manager.updateTask(taskOne);
         manager.updateEpicTask(epicTaskOne);
         manager.updateSubTask(subtaskOne);
-
-        System.out.println("хранилище после обновления - " + manager.getListOfTasks());
 
         ArrayList<Task> test = manager.getListOfTasks();
         ArrayList<Task> test2 = manager.getListOfEpicTasks();
@@ -105,16 +102,31 @@ public class Main {
 
         System.out.println("тестируем получение по id 2 " + manager.getTaskById(2));
         System.out.println("История просмотров задач:\n" + manager.getHistory());
-        System.out.println("тестируем получение по id 4 " + manager.getEpicTaskById(3));
+        System.out.println();
+        System.out.println("тестируем получение по id 3 " + manager.getEpicTaskById(3));
         System.out.println("История просмотров задач:\n" + manager.getHistory());
-        System.out.println("тестируем получение по id 1 " + manager.getSubTaskById(4));
+        System.out.println();
+        System.out.println("тестируем получение по id 4 " + manager.getSubTaskById(4));
+        System.out.println("История просмотров задач:\n" + manager.getHistory());
+        System.out.println();
+        System.out.println("тестируем получение по id 2 " + manager.getTaskById(2));
+        System.out.println("История просмотров задач:\n" + manager.getHistory());
+        System.out.println();
+        System.out.println("тестируем получение по id 3 " + manager.getEpicTaskById(3));
+        System.out.println("История просмотров задач:\n" + manager.getHistory());
+        System.out.println();
+        System.out.println("тестируем получение по id 4 " + manager.getSubTaskById(4));
         System.out.println("История просмотров задач:\n" + manager.getHistory());
         System.out.println();
 
-        System.out.println("дальше удаляем по номеру");
+        System.out.println("дальше удаляем по номеру задачу(2)");
         manager.deleteTaskById(2);
+        System.out.println("История просмотров задач после удаления задачи(2):\n" + manager.getHistory());
+        System.out.println();
+
+        System.out.println("дальше снова удаляем по номеру эпик(1, id3)");
         manager.deleteEpicTaskById(3);
-        manager.deleteSubTaskById(4);
+        System.out.println("История просмотров задач после удаления эпика(1, id3), с ней должна удалиться ее подзадача1:\n" + manager.getHistory());
 
         System.out.println("тестируем получение по id 2 после удаления по номеру " + manager.getTaskById(2));
         System.out.println("тестируем получение по id 3 после удаления по номеру " + manager.getEpicTaskById(3));
@@ -136,9 +148,6 @@ public class Main {
         ArrayList<Task> test8 = manager.getListOfEpicTasks();
         ArrayList<Task> test9 = manager.getListOfSubTasks();
         System.out.println("получение списка после удаления - " + "\n" + test7 + "\n" + test8 + "\n" + test9);
-
-        System.out.print("если удалить несуществующую задачу - ");
-        manager.deleteTaskById(6);
 
 
     }
---
+++ src/managers/Node.java
@@ -0,0 +1,15 @@
+package managers;
+
+import tasks.Task;
+
+public class Node<Task> {
+    public Task data;
+    public Node<Task> next;
+    public Node<Task> prev;
+
+    public Node(Node<Task> prev, Task data, Node<Task> next) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}

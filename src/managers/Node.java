18451563d856b1f12e3934adda087d6ccc2cc569
biscuitka <biscuitka@ya.ru>
2023-03-01 10:19:18 +0300

    сделать приватными поля, добавить гет и сет, убрать null

--- src/managers/Node.java
+++ src/managers/Node.java
@@ -1,17 +1,38 @@
 package managers;
 
+import tasks.Task;
+
 /**
  * класс узла списка задач
- * @param <Task>
  */
-public class Node<Task> {
-    public Task data;
-    public Node<Task> next;
-    public Node<Task> prev;
+public class Node {
+    private final Task data;
+    private Node next;
+    private Node prev;
 
-    public Node(Node<Task> prev, Task data, Node<Task> next) {
+    public Node(Node prev, Task data, Node next) {
         this.data = data;
-        this.next = null;
-        this.prev = null;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public Task getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
     }
 }

4bc1421e06c43fed78a6a60167999c88fb3a676f
biscuitka <biscuitka@ya.ru>
2023-04-17 19:10:49 +0300

    удалить ненужный импорт

--- test/tests/HttpTaskManagerTest.java
+++ test/tests/HttpTaskManagerTest.java
@@ -1,24 +1,11 @@
 package tests;
 
 import api.HttpTaskManager;
-import api.HttpTaskServer;
 import api.KVServer;
-import managers.Managers;
-import managers.TaskManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import tasks.Epic;
-import tasks.Subtask;
-import tasks.Task;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class HttpTaskManagerTest extends TaskManagerTest<HttpTaskManager>{
     KVServer kvServer;
